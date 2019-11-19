2bee39e50b8da2c8fa940088041dce341bc8296c
Taeho Kim <56941469+kim4t@users.noreply.github.com>
2019-11-19 17:05:51 -0500

    Add files via upload

--- FinalProject/src/Board.java
+++ FinalProject/src/Board.java
@@ -1,12 +1,36 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Board {
-	private static Board board=null;
+	List<Propertiy> pList = new ArrayList<>();
+	List<Player> uList = new ArrayList<Player>();
+	Player p = new Player();
+	private int numberOfPlayers = 0;
 	
-	public static Board getInstance() {
-		if(board==null) {
-			board=new Board();
+
+	public void rollDice() {
+		Random r = new Random();
+		int d1 = r.nextInt(6) + 1;
+		int d2 = r.nextInt(6) + 1;
+		p.movePlayer(d1+d2);
+		//Player action();
+		if (d1 == d2 && d1 == 6) {
+			rollDice();
 		}
-		return board;
+
 	}
-	
+
+	public void popluateProperties() throws FileNotFoundException, IOException {
+		ObjectInputStream readProperties = new ObjectInputStream(new FileInputStream("properties.txt"));
+	}
+
+	public void printBoard() {
+
+	}
+
 }
